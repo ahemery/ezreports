4af541d45f1b05784828cf11b81470cba2b65a42
Adrien <adrien.hemery@free.fr>
2016-10-13 16:17:58 +0200

    update...

--- ezreports/wsgi.py
+++ ezreports/wsgi.py
@@ -7,9 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+
+sys.path.append('/var/www/ezreports/')
+sys.path.append('/var/www/ezreports/p3/lib/python3.4/site-packages')
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ezreports.settings")
 
